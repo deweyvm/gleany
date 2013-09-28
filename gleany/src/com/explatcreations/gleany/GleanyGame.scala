c3ab3ee973d4d734959fe97ce49ea86d2fe28963
deweyvm <explatcreations@gmail.com>
2013-09-28 09:57:22 -0400

    updated libraries

--- gleany/src/com/explatcreations/gleany/GleanyGame.scala
+++ gleany/src/com/explatcreations/gleany/GleanyGame.scala
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.explatcreations.gleany.saving.{VideoSettings, AudioSettings}
 
 object Gleany {
-
+    //var gleany:Gleany = null
 }
 
 class Gleany(val settings:AudioSettings with VideoSettings) {
@@ -17,19 +17,24 @@ object GleanyGame {
         Gdx.app.exit()
     }
 
+    /**
+     * this function does not return
+     */
     def runGame(config:GleanyConfig, game:GleanyGame) {
         new LwjglApplication(game, config.toLwjgl)
     }
 }
 
-abstract class GleanyGame extends ApplicationListener {
+abstract class GleanyGame/*(initializer:GleanyInitializer)*/ extends ApplicationListener {
 
     def update()
     def draw()
 
     override def create() {
         Gdx.app.setLogLevel(Application.LOG_NONE)
+        //Gleany.gleany = initializer.create
     }
+
     override def render() {
         update()
         draw()

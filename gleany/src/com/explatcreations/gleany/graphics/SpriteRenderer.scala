68ac79ebd50b7f9456cfb6e2c5c2b80fb1610509
deweyvm <explatcreations@gmail.com>
2013-09-30 10:17:35 -0400

    working on sprite renderer

--- gleany/src/com/explatcreations/gleany/graphics/SpriteRenderer.scala
+++ gleany/src/com/explatcreations/gleany/graphics/SpriteRenderer.scala
@@ -1,8 +1,26 @@
 package com.explatcreations.gleany.graphics
 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.{Sprite, SpriteBatch}
+import scala.collection.mutable.ArrayBuffer
 
 class SpriteRenderer {
     val batch = new SpriteBatch(10000)
 
+    var draws:ArrayBuffer[() => Unit] = ArrayBuffer[() => Unit]()
+
+    def addCall(call:() => Unit) {
+        draws += call
+    }
+
+    def draw(sprite:Sprite) {
+        addCall (() => sprite.draw(batch))
+    }
+
+    /*def draw(fontCache:HackFontCache, start:Int, end:Int) {
+        addCall(() => fontCache.draw(batch, start, end))
+    }
+
+    def draw(fontCache:HackFontCache) {
+        addCall(() => fontCache.draw(batch))
+    }*/
 }
